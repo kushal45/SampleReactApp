67db089394e92355db549f2b256e2c2c314ab2bb
kushal878 <kushal@curiotechnologies.in>
2021-06-26 19:14:34 +0530

    This is initial commit which contains solution for multiple Expense Items within Expenses Component

---
+++ src/components/Expenses.js
@@ -0,0 +1,18 @@
+import ExpenseItem from "./ExpenseItem";
+function Expenses(props) {
+    return (
+        <div>
+            {
+            props.expenses.map(expense => (
+                <ExpenseItem
+                    title={expense.title}
+                    amount={expense.amount}
+                    date={expense.date}
+                />
+                ))}
+        </div>
+
+    );
+}
+
+export default Expenses;
